ab9a313fb0b645a5404f9f320948ac478eab4f05
Medha Narayan <medhanarayanccs@gmail.com>
2023-11-07 01:42:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import WordCounter from './components/WordCounter';
+
+const App = () => {
+  return (
+    <div>
+      <WordCounter />
+    </div>
+  );
+};
+
+export default App;
